d870b4c76502d909c846faa0d324e2fa33b22e0e
Suraj Muraleedharan <surajcm@gmail.com>
2020-07-26 08:02:49 +0530

    Adding first juint, jacoco coverage and pmd

---
+++ src/test/java/com/online/travel/dataaccess/serialization/LocalDateAdapterTest.java
@@ -0,0 +1,34 @@
+package com.online.travel.dataaccess.serialization;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+class LocalDateAdapterTest {
+    private static final LocalDateAdapter LOCAL_DATE_ADAPTER = new LocalDateAdapter();
+
+    @Test
+    public void successfulUnmarshal() throws Exception {
+        String dateVal = "2012-08-19";
+        LocalDate convertedDate = LOCAL_DATE_ADAPTER.unmarshal(dateVal);
+        Assertions.assertEquals(2012, convertedDate.getYear());
+        Assertions.assertEquals(8, convertedDate.getMonthValue());
+        Assertions.assertEquals(19, convertedDate.getDayOfMonth());
+    }
+
+    @Test
+    public void failedUnmarshal() {
+        String dateVal = "2012-18-19";
+        Assertions.assertThrows(DateTimeParseException.class, () -> LOCAL_DATE_ADAPTER.unmarshal(dateVal));
+    }
+
+    @Test
+    public void successfulMarshal() throws Exception {
+        LocalDate date = LocalDate.of(2012,8,22);
+        String convertedDate = LOCAL_DATE_ADAPTER.marshal(date);
+        Assertions.assertEquals("2012-08-22", date.toString());
+    }
+
+}
